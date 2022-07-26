292e3832ca1dfd9e38dcda1aff6d3bf2cdf319da
varun <foo@gmail.com>
2022-07-26 20:09:38 +0530

    Create model for User table.

--- login-backend/login_signup/api/user/models.py
+++ login-backend/login_signup/api/user/models.py
@@ -1,3 +1,29 @@
+"""
+author: Varun
+"""
+
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """User model"""
+
+    first_name = models.CharField(max_length=80, blank=False)
+    last_name = models.CharField(max_length=80, blank=True)
+    email = models.EmailField(max_length=100, blank=False)
+    password = models.CharField(max_length=200, blank=False)
+    phone_number = models.CharField(max_length=12, blank=True)
+    session_token = models.CharField(max_length=30, default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        """Additional settings"""
+
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+        db_table = 'user'
